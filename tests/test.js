5144efa15d8ea17555aa61958f89470d6557c642
Nilton Cesar <niltoncms@gmail.com>
2017-01-31 19:22:57 -0300

    Cover scenario where there's no explicit 'zero integer' part.

--- tests/test.js
+++ tests/test.js
@@ -9,3 +9,14 @@ describe('Transform numbers between standards', function() {
 		expect(n.toInternational('123.456,789')).toEqual(123456.789);
 	});
 });
+
+
+describe('Cases with no explicit integer part', function() {
+	it('When a number has no \'0\' before the .', function() {
+		expect(n.toBrazilian(.333)).toEqual('0,333')
+	});
+
+	it('When a formated number has no \'0\' before the ,', function() {
+		expect(n.toInternational(',333')).toEqual(0.333)
+	});
+});
