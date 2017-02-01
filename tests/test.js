3cdc550f8f0133a6eca9b825bf36b7e3c2859ec8
Nilton Cesar <niltoncms@gmail.com>
2017-01-31 23:32:06 -0300

    Add input type checking coverage tests.

--- tests/test.js
+++ tests/test.js
@@ -20,3 +20,73 @@ describe('Cases with no explicit integer part', function() {
 		expect(n.toInternational(',333')).toEqual(0.333)
 	});
 });
+
+
+describe('Cases when the input isn\'t of the expected type', function() {
+	it('When converting to international, the expected input must a string', function() {
+
+		var string = '',
+				number = 0,
+				object = {},
+				bool = true,
+				f = function(){};
+
+		expect(function() {
+			n.toInternational(string)
+		}).not.toThrow(new TypeError('The input must be a string.'));
+
+		expect(function() {
+			n.toInternational(number)
+		}).toThrow(new TypeError('The input must be a string.'));
+
+		expect(function() {
+			n.toInternational(object)
+		}).toThrow(new TypeError('The input must be a string.'));
+
+		expect(function() {
+			n.toInternational(bool)
+		}).toThrow(new TypeError('The input must be a string.'));
+
+		expect(function() {
+			n.toInternational(f)
+		}).toThrow(new TypeError('The input must be a string.'));
+
+		expect(function() {
+			n.toInternational(undefined)
+		}).toThrow(new TypeError('The input must be a string.'));
+	});
+
+
+	it('When converting to brazilian, the expected input must a number', function() {
+
+		var number = 0,
+				string = '',
+				object = {},
+				bool = true,
+				f = function(){};
+
+		expect(function() {
+			n.toBrazilian(number)
+		}).not.toThrow(new TypeError('The input must be a number.'));
+
+		expect(function() {
+			n.toBrazilian(string)
+		}).toThrow(new TypeError('The input must be a number.'));
+
+		expect(function() {
+			n.toBrazilian(object)
+		}).toThrow(new TypeError('The input must be a number.'));
+
+		expect(function() {
+			n.toBrazilian(bool)
+		}).toThrow(new TypeError('The input must be a number.'));
+
+		expect(function() {
+			n.toBrazilian(f)
+		}).toThrow(new TypeError('The input must be a number.'));
+
+		expect(function() {
+			n.toBrazilian(undefined)
+		}).toThrow(new TypeError('The input must be a number.'));
+	});
+});
