4f1ace2d442ad6d35b59227ef679b7fa8a753b58
Nilton Cesar <niltoncms@gmail.com>
2017-02-07 14:21:20 -0300

    Apply some best practices in code style.

--- src/numeros.js
+++ src/numeros.js
@@ -1,55 +1,56 @@
 ;(function(root, factory) {
 
-  if(typeof define === 'function' && define.amd)
-		define('n', factory);
+  if(typeof define === 'function' && define.amd) define('n', factory);
 
-  else if(typeof exports === 'object')
-		module.exports = factory();
+  else if(typeof exports === 'object') module.exports = factory();
 
   else root.n = factory();
 
 })(this, function() {
 
-	// Make brazilian numbers standard out of internation standard, for viewing
+	// Make brazilian numbers standard out of international standard, for viewing
 	var toBrazilian = function(internationalDecimal) {
 		if(typeof(internationalDecimal) !== 'number')
 			throw new TypeError('The input must be a number.');
 
-		var stringedNumber =
-			// In case the number isn't an actual number
-			internationalDecimal.toString()
-			// Replace the decimal dot for comma
-			.replace(/\./, ',');
+		// Make the number malleable and replace the decimal dot for comma
+		var stringedNumber = internationalDecimal.toString().replace(/\./, ','),
 
-		var integerPart = stringedNumber.substring(0, stringedNumber.search(','));
+			// Get the integet part of the original number
+			integerPart = stringedNumber.substring(0, stringedNumber.search(',')),
+
+			integerPartSize = integerPart.length;
 
 		// If the integer part has more than three characters, separate them by dots
-		if(integerPart.length > 3) {
-			var dottedIntegerPart = new String;
+		if(integerPartSize > 3) {
+			var dottedIntegerPart = '';
 
-			for(
-				var i = 0, j = 0;
-				i < Math.floor((integerPart.length/3));
-				) {
+			for(var actualLoopIteration = 0,
+							previousLoopIteration = 0,
+							threeDigitsBlock = Math.floor(integerPartSize/3);
+					actualLoopIteration < threeDigitsBlock;) {
 
-				// Control variable to indicate loop stage
-				i++;
+				actualLoopIteration += 1;
 
 				// Summing up the next three digits block
-				dottedIntegerPart = integerPart.slice(integerPart.length-(i*3),integerPart.length-(j*3)) + dottedIntegerPart;
+				dottedIntegerPart =
+					integerPart.slice(
+						integerPartSize-(actualLoopIteration*3),
+						integerPartSize-(previousLoopIteration*3)
+					) + dottedIntegerPart;
 
 				// When it's the last iteration, break the loop
-				if((integerPart.length)-(i*3) === 0) break;
+				if((integerPartSize)-(actualLoopIteration*3) === 0) break;
 				// If it isn't the last iteration, put a dot behind the last added digits
 				else dottedIntegerPart = '.' + dottedIntegerPart;
 
-				// Control variable to keep state of the previous iteration
-				j++;
+				previousLoopIteration += 1;
 			}
 
 			// If integer length isn't multiple of three, add to its start the missing
-			if(integerPart.length/3 !== 0)
-				dottedIntegerPart = integerPart.slice(0, integerPart.length%3) + dottedIntegerPart;
+			if(integerPartSize/3 !== 0)
+				dottedIntegerPart = 
+          integerPart.slice(0, integerPartSize%3) + dottedIntegerPart;
 
 			// Return the integer part + comma and decimal part
 			return dottedIntegerPart + stringedNumber.slice(stringedNumber.search(','));
@@ -64,11 +65,8 @@
 		if(typeof(brazilianDecimal) !== 'string')
 			throw new TypeError('The input must be a string.');
 
-		return Number(brazilianDecimal
-			// Remove the number separation dots
-			.replace(/\./g, '')
-			// Replace the decimal comma for dot
-			.replace(/,/, '.'));
+		// Remove the number separation dots and replace the decimal comma for dot
+		return Number(brazilianDecimal.replace(/\./g, '').replace(/,/, '.'));
 	};
 
 
